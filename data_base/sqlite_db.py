429c5dc12c11fe4d2239c150893721b6d1fe4b49
L <102589782+skotchL@users.noreply.github.com>
2022-06-30 12:11:09 +0500

    Add files via upload

---
+++ data_base/sqlite_db.py
@@ -0,0 +1,20 @@
+import sqlite3 as sq
+from create_bot import bot
+
+def sql_start():
+	global base, cur
+	base = sq.connect('KFC_malumot.db')
+	cur = base.cursor()
+	if base:
+		print('Data base connected OK!')
+	base.execute('CREATE TABLE IF NOT EXISTS menu(img TEXT, name TEXT PRIMARY KEY, description TEXT, price TEXT)')
+	base.commit()	
+
+async def sql_add_command(state):
+	async with state.proxy() as data:
+		cur.execute('INSERT INTO menu VALUES (?, ?, ?, ?)', tuple(data.values()))
+		base.commit()
+
+async def sql_read(message):
+	for ret in cur.execute('SELECT * FROM menu').fetchall():
+		await bot.send_photo(message.from_user.id, ret[0], f'{ret[1]}\nMalumot: {ret[2]}\nNarxi: {ret[-1]}')
